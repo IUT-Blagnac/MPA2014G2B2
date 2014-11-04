0b883b286520967b91805a4ee424b4b049993aab
Simon Prévidente <simon.previdente@free.fr>
2014-11-04 16:14:21 +0100

    Debug createHTML

--- sprint_5/src/View/createHTML.java
+++ sprint_5/src/View/createHTML.java
@@ -7,7 +7,7 @@ public class createHTML {
 	public createHTML(){
 	// Initialisation de la fenetre du programme
 		Controller controller = new Controller();
-		OPTIlib.rechargeIt(controller);
+		OPTIlib.findFiles("data", controller);
 		HTMLlib.htmlGeneratePage(controller);
 	}
 	public static void main(String[] args) {
